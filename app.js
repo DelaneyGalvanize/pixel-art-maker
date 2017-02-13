09cc48e5194b808133c84a37c79a1fdc69ff44a2
DelaneyGalvanize <delaney.patterson001@gmail.com>
2017-02-13 15:05:42 -0700

    more css changes

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
 
 
+
 var body=document.getElementsByTagName('body')[0]
+var title=document.createElement('h1')
+title.innerText= 'Lets Make Some Art!'
+body.appendChild(title)
+
+
 var boxContainer= document.createElement('div')
 boxContainer.className= 'container'
 body.appendChild(boxContainer)
 
-var title=document.createElement('h1')
-boxContainer.appendChild(title)
-title.innerText= 'Lets Make Some Art!'
 
-for (var i=0; i < 800; i++) {
+for (var i=0; i < 646; i++) {
   var div=document.createElement('div')
   boxContainer.appendChild(div)
   div.className='pixel'
 }
 
-var pixel= document.getElementsByClassName('pixel')
-
+// var pixel= document.getElementsByClassName('pixel')
+// boxContainer.appendChild(pixel)
 var palette= document.createElement('div')
 palette.className= 'palette'
-palette.style.height='695px';
-palette.style.width= '125px';
-palette.style.border= '3px solid black';
-palette.style.position= 'absolute';
-palette.style.display= 'inline-block';
-// palette.style.float= 'right';
-palette.style.margin= '80px 0 0 0'
-body.appendChild(palette)
+palette.style.height='50px';
+palette.style.width= '925px';
+// palette.style.border= '2px solid black';
+// palette.style.borderRadius: '20px 40px 20px 40px'
+palette.style.position= 'none';
+palette.style.display= 'inline';
+// palette.style.float= 'left';
+// palette.style.margin= '0 0 80px 0'
+body.appendChild(palette);
+
+
+
 
 
 
@@ -35,29 +42,37 @@ var colors= ['maroon','red','pink', 'deeppink','salmon','orange', 'yellow', 'gol
 for (var i = 0; i < 22; i++) {
   var div=document.createElement('div')
   let id= div.id=colors[i]
-  div.style.height='61px';
-  div.style.width='60px';
+  div.style.height='53px';
+  div.style.width='53px';
   div.style.border='1.5px solid black';
   div.style.display= 'inline-block';
   div.style.borderRadius= '50px 50px 50px 50px'
   div.style.backgroundColor= colors[i]
-  div.style.float= 'left';
-
+  // div.style.float= 'left';
   palette.appendChild(div)
 }
 
 
+
+
 function picked () {
-  let pick= event.target
-  let pickedColor= pick.style.backgroundColor
+  var pick= event.target
+  var pickedColor= pick.style.backgroundColor
   console.log(pickedColor);
   return pickedColor
 }
 
 palette.addEventListener('click', function(){
-  let coloring=picked()
+  var coloring=picked()
 
   boxContainer.addEventListener('click', function(){
     event.target.style.backgroundColor=coloring
   })
 })
+
+
+  // clearButton.addEventListener('click', function(){
+    // //Hide colors
+    // for (var i = 0; i < pixel.length; i++) {
+    // // var coloring= picked()
+    // pixel[i].value= '';
